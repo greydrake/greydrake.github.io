911952cedba53339701cc5d7a0a58304ef8fa3c6
greydrake <32744605+greydrake@users.noreply.github.com>
2018-05-10 10:13:43 -0700

    p5sketch

---
+++ js/p5sketch.js
@@ -0,0 +1,16 @@
+function setup() {
+  // put setup code here
+  background(5);
+  createCanvas(windowWidth,windowHeight);
+noStroke();
+
+  fill(79,19,156,4);
+}
+
+function draw() {
+  // put drawing code here
+
+
+  
+  ellipse(pmouseX, pmouseY, 20, 20);
+}
