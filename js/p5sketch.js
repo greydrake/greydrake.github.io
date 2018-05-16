70c0eb02ef576d6461e349914b5cbb58eeabbf09
greydrake <32744605+greydrake@users.noreply.github.com>
2018-05-15 22:02:40 -0700

    p5sketch

--- js/p5sketch.js
+++ js/p5sketch.js
@@ -1,26 +1,4 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title> Net Art</title>
-	
-<link rel="stylesheet" type="text/css" href="css/mystyle.css">
 
-</head>
-
-<body class="black">
-<ul>
-		<li><a href="index.html">Home</a></li>
-		<li><a href="about.html">About</a></li>
-		<li><a href="portfolio.html">Portfolio</a></li>
-		<li><a href="contact.html">Contact</a></li>
-		<li><a href="Processing.html">Processing</a></li>
-		<li><a href="Net Art.html">Net Art</a></li>
-		<li><a href="p5sketch.html">p5 Excercise</a></li>
-	</ul>
-
-<h1>p5sketch</h1>
- <!--
- <script>
 
 function setup() {
   // put setup code here
@@ -38,7 +16,5 @@ function draw() {
   
   ellipse(pmouseX, pmouseY, 20, 20);
 }
-</script>
--->
-</body>
-</html>
+
+
